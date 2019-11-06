0930ba56c148db702c8c52a6d2dafc0771138ba3
Rafal Szczotka <rafalszczotka@wp.pl>
2019-11-06 18:58:03 +0000

    init

---
+++ src/main/java/com/filesheriff/FilesheriffApplication.java
@@ -0,0 +1,13 @@
+package com.filesheriff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesheriffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesheriffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filesheriff/FilesheriffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filesheriff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesheriffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
